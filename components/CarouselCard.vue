5819e5df953b60f1f267af15594d8f45949d0c44
tatall <tatall@ttu.ee>
2023-03-14 19:42:08 +0200

    cards now have urls

--- components/CarouselCard.vue
+++ components/CarouselCard.vue
@@ -9,19 +9,26 @@
             <img class="object-cover " src="/Vector_7.svg" />
             <div class="flex flex-col h-full p-3 bg-light_purple gap-2">
                 <h3 class=" text-3xl font-medium">{{ title }}</h3>
-                <p class="text-slate-100 ">{{ description }}</p>
-                <NuxtLink class="text-white underline hover:text-slate-100" to="/info">View Project <svg
-                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            <p class="text-slate-100 ">{{ description }}</p>
+            <!-- <NuxtLink class="text-white underline hover:text-slate-100"
+                    to="/info">View Project <svg xmlns="http://www.w3.org/2000/svg"
+                        width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                        stroke-linecap="round" stroke-linejoin="round" class="inline">
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                                                                <polyline points="12 5 19 12 12 19"></polyline>
+                                                            </svg> </NuxtLink> -->
+                <a class="text-white underline hover:text-slate-100" :href=url target="_blank">View Project
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                         class="inline">
                         <line x1="5" y1="12" x2="19" y2="12"></line>
                         <polyline points="12 5 19 12 12 19"></polyline>
-                    </svg> </NuxtLink>
+                    </svg> </a>
+
             </div>
 
         </div>
     </div>
-
 </template>
 
 <script>
@@ -36,6 +43,10 @@ export default {
             required: true
         },
         description: {
+            type: String,
+            required: true
+        },
+        url: {
             type: String,
             required: true
         }
--- components/ThreeDCaroussel.vue
+++ components/ThreeDCaroussel.vue
@@ -3,38 +3,41 @@
 
 
 <template>
-
     <div class="carousel-container">
         <carousel-3d :controls-visible="true" :controls-prev-html="'&#10092; '" :controls-next-html="'&#10093;'"
-            :autoplay="false" :autoplay-timeout="4000" :count="6" :space="300" :height="500">
+            :autoplay="false" :autoplay-timeout="4000" :count="7" :space="300" :height="500">
             <slide :index="0" :key="0">
                 <CarouselCard imageUrl="/eviikingidViking.png" title="E-Viikingid"
-                    description="A visually beatiful webpage made in wordpress." />
+                    description="A visually beatiful webpage made in wordpress." url="http://viikingid.unaux.com/" />
             </slide>
             <slide :index="1" :key="1">
                 <CarouselCard imageUrl="/viking-logo-Transparent.png" title="Viiking"
-                    description="A single page webapp made purely in css, html, javascript." />
+                    description="A single page webapp made purely in css, html, javascript."
+                    url="https://www.viiking.tk/" />
             </slide>
             <slide :index="2" :key="2">
-                <CarouselCard imageUrl="/toboTaxi.jpg" title="Tobo"
-                    description="An modified version of HTML5UP template." />
+                <CarouselCard imageUrl="/toboTaxi.jpg" title="Tobo" description="An modified version of HTML5UP template."
+                    url="https://simplytobo.eu/" />
             </slide>
             <slide :index="3" :key="3">
-                <CarouselCard imageUrl="/shrekRacing.png" title="World Rally Shrek"
+                <CarouselCard imageUrl="/shrekRacing.png" title="World Rally Shrek" url="https://shrekracing.tt23.repl.co/"
                     description='A modified version of the website "Cyberpunk 2077".' />
             </slide>
             <slide :index="4" :key="4">
-                <CarouselCard imageUrl="/rockRockStore.jpg" title="Väekivid"
+                <CarouselCard imageUrl="/rockRockStore.jpg" title="Väekivid" url="https://vaekivid.simplytobo.eu/"
                     description="An online shop made with React.js, with working state managment and payments." />
             </slide>
 
             <slide :index="5" :key="5">
-                <CarouselCard imageUrl="/cibus.jpg" title="Cibus"
+                <CarouselCard imageUrl="/cibus.jpg" title="Cibus" url="https://cibus.tt23.repl.co/"
                     description="A single page webapp with Google login and Supabase as database." />
+            </slide>
+            <slide :index="6" :key="6">
+                <CarouselCard imageUrl="/kahoot.jpg" title="Katoot" url="https://replit.com/@TT23/kursbuch3"
+                    description="Yes, I made my own server and website from scratch, just to play one game of katoot ." />
             </slide>
         </carousel-3d>
     </div>
-
 </template>
 
 <script>
